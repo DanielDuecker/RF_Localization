1af62d4e34c476938f34c2b609384894c4e0239c
Daniel Duecker <daniel.duecker@tuhh.de>
2016-12-06 17:00:09 +0100

    minor changes

--- test_gantry.py
+++ test_gantry.py
@@ -4,5 +4,5 @@ import gantry_control
 gt = gantry_control.GantryControl()
 
 # gantry_control.wp_generator('wp_list.txt')
-gt.start_LocEar(centerfreq=433.9e6)
+gt.start_LocEar(freqtx=[433.7e6, 433.9e6])
 gt.process_measurement_sequence('wp_list.txt', 'measdata.txt')
