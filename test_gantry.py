32f6bcabc9dfdc77ef573de954225f51b5e4ad65
Daniel Duecker <daniel.duecker@tuhh.de>
2017-01-11 12:00:14 +0100

    minor changes

--- test_gantry.py
+++ test_gantry.py
@@ -5,12 +5,13 @@ import numpy as np
 
 gt = gantry_control.GantryControl()
 
-# rf_tools.wp_generator('wp_list_2017_01_10.txt', [0, 0], [2900, 1500], [59, 31], 10.0, True) # x-axis is along belt-drive
+rf_tools.wp_generator('wp_list_2017_01_11.txt', [0, 0], [2900, 1500], [59, 31], 10.0, True) # x-axis is along belt-drive
 
 freqtx = [433.9e6, 434.1e6, 434.3e6, 434.5e6]
 
-gt.follow_wp_trajectory(1000, 5000, 100)
-#gt.start_CalEar(freqtx)
-#gt.process_measurement_sequence('wp_list_2017_01_10.txt', 'measdata_2017_01_10.txt')
+#gt.follow_wp_trajectory(1000, 5000, 100)
+
+gt.start_CalEar(freqtx)
+gt.process_measurement_sequence('wp_list_2017_01_11.txt', 'measdata_2017_01_11.txt')
 
 
