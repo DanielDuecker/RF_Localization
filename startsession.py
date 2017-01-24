457e8a30e4eafcf1542cf7242506ee96b9add1b6
Daniel Duecker <daniel.duecker@tuhh.de>
2017-01-24 18:27:01 +0100

    minor changes

--- startsession.py
+++ startsession.py
@@ -1,4 +1,4 @@
-#import rf
+import rf
 import rf_tools
 import numpy as np
 
@@ -9,8 +9,7 @@ txpos = [[0, 0],  # 433.9MHz
          [0, 800],  # 434.1MHz
          [1270, 50],  # 434.3MHz
          [1270, 750]]  # 434.5MHz
-rf_tools.analyse_measdata_from_file('measdata_2017_01_10.txt', txpos, txpos_offset, freqtx)
-
+#rf_tools.analyse_measdata_from_file('measdata_2017_01_10.txt', txpos, txpos_offset, freqtx)
 
 
 """
@@ -34,20 +33,20 @@ alpha = [0.0133680937013, 0.0129727376314, 0.0139948951361, 0.0124357921976]
 xi = [-10.9915388063, -8.40605996688, -9.67729971289, -8.34912917588]
 """
 
-#cal = rf.CalEar(freqtx)
+cal = rf.CalEar(freqtx)
 
 #cal.plot_psd()
 #cal.get_performance()
-#cal.plot_txrss_live()
+cal.plot_txrss_live()
 
 
 freqspan = 2e4
 freqcenter = np.mean(freqtx)
 
 
-#loc = rf.LocEar(freqtx, freqspan, alpha, xi, txpos)
+#loc = rf.LocEar(freqtx, freqspan, alpha, xi, txpos+txpos_offset)
 #loc.plot_txdist_live()
 
 x0 = np.array([300, 400])  # initial estimate
 
-#loc.map_path_ekf(x0, False, False, True)
+#loc.map_path_ekf(x0, True, False, False)
