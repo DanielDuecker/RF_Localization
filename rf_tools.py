0075b4bca8526de78d6ebbcb9acc071c4b04d0b1
DanielDuecker <daniel.duecker@tuhh.de>
2017-04-27 20:34:15 +0200

    analyzation tool can now handle sparse measurement grids

--- rf_tools.py
+++ rf_tools.py
@@ -130,6 +130,7 @@ def write_measfile_header(ofile, file_description, x0, xn, grid_dxdy, timemeas, 
     return True
 
 
+
 def analyze_measdata_from_file(analyze_tx=[1,2,3,4], meantype='db_mean'):
     """
 
@@ -143,7 +144,6 @@ def analyze_measdata_from_file(analyze_tx=[1,2,3,4], meantype='db_mean'):
 
     measdata_filename = hc_tools.select_file()
 
-
     with open(measdata_filename, 'r') as measfile:
         load_description = True
         load_grid_settings = False
@@ -151,6 +151,9 @@ def analyze_measdata_from_file(analyze_tx=[1,2,3,4], meantype='db_mean'):
         meas_data_append_list = []
 
         plotdata_mat_lis = []
+
+        totnumwp = 0
+        measured_wp_list = []
 
         for i, line in enumerate(measfile):
 
@@ -207,20 +210,35 @@ def analyze_measdata_from_file(analyze_tx=[1,2,3,4], meantype='db_mean'):
                 print('tx_pos = ' + str(txpos_list))
                 print('freqtx = ' + str(freqtx))
 
+                startx = x0[0]
+                endx = xn[0]
+                stepx = data_shape_file[0]
+
+                starty = x0[1]
+                endy = xn[1]
+                stepy = data_shape_file[1]
+
+                xpos = np.linspace(startx, endx, stepx)
+                ypos = np.linspace(starty, endy, stepy)
+
+
+                wp_matx, wp_maty = np.meshgrid(xpos, ypos)
+
+                #print(xpos)
+
             if load_measdata and not load_grid_settings:
                 #print('read measdata')
+
+                totnumwp += 1
                 meas_data_line = map(float, line[0:-3].split(', '))
                 meas_data_append_list.append(meas_data_line)
 
-
                 meas_data_mat_line = np.asarray(meas_data_line)
 
-                num_wp = int(meas_data_mat_line[2])
+                measured_wp_list.append(int(meas_data_mat_line[2]))
                 num_tx = int(meas_data_mat_line[3])
                 num_meas = int(meas_data_mat_line[4])
 
-                # @todo add numtx to data file
-                freq_vec = freqtx
 
                 first_rss = 5 + num_tx
 
@@ -240,20 +258,29 @@ def analyze_measdata_from_file(analyze_tx=[1,2,3,4], meantype='db_mean'):
                     # print('var = ' + str(var))
                 wp_pos = [meas_data_mat_line[0], meas_data_mat_line[1]]
 
-                plotdata_line = np.concatenate((wp_pos, mean, var), axis=1)
+                #print(str(wp_pos))
+                #print(str(mean))
+                #print(str(var))
+                #print(str(np.concatenate((wp_pos, mean, var), axis=0)))
+                plotdata_line = np.concatenate((wp_pos, mean, var), axis=0)
+
 
                 plotdata_mat_lis.append(plotdata_line)
+                #print (str(plotdata_mat_lis))
+
+
 
         measfile.close()
 
         # print('shape_from_file ' + str(data_shape_file))
         data_shape = [data_shape_file[1], data_shape_file[0]]
-        data_shape = [9, 16]
+
         # print('shape : ' + str(np.shape(x)))
 
-        totnumwp = data_shape[0] * data_shape[1] #num_wp + 1  # counting starts with zero
+        # totnumwp = data_shape[0] * data_shape[1] #num_wp + 1  # counting starts with zero
 
         plotdata_mat = np.asarray(plotdata_mat_lis)
+        #print(str(plotdata_mat))
         #print('Number of gridpoints: ' + str(plotdata_mat.shape[0]))
 
 
@@ -262,9 +289,9 @@ def analyze_measdata_from_file(analyze_tx=[1,2,3,4], meantype='db_mean'):
         Model fit
         """
 
-        def rsm_model(dist, alpha, gamma):
+        def rsm_model(dist_rsm, alpha_rsm, gamma_rsm):
             """Range Sensor Model (RSM) structure."""
-            return -20 * np.log10(dist) - alpha * dist - gamma  # rss in db
+            return -20 * np.log10(dist_rsm) - alpha_rsm * dist_rsm - gamma_rsm  # rss in db
 
         alpha = []
         gamma = []
@@ -297,33 +324,32 @@ def analyze_measdata_from_file(analyze_tx=[1,2,3,4], meantype='db_mean'):
         x = plotdata_mat[:, 0]
         y = plotdata_mat[:, 1]
 
-
-
         fig = plt.figure(6)
         for itx in analyze_tx:
             pos = 221 + itx
             if len(analyze_tx) == 1:
                 pos = 111
 
-
-
             ax = fig.add_subplot(pos)
             rss_mean = plotdata_mat[:, 2 + itx]
             rss_var = plotdata_mat[:, 2 + num_tx + itx]
 
+            rss_mat_ones = np.ones(np.shape(wp_matx)) * (-200)  # set minimum value for not measured points
+            rss_full_vec = np.reshape(rss_mat_ones, (len(xpos) * len(ypos), 1))
 
+            measured_wp_list = np.reshape(measured_wp_list, (len(measured_wp_list), 1))
+            rss_mean = np.reshape(rss_mean, (len(rss_mean), 1))
 
+            rss_full_vec[measured_wp_list, 0] = rss_mean
 
+            rss_full_mat = np.reshape(rss_full_vec, data_shape)
 
+            # mask all points which were not measured
+            rss_full_mat = np.ma.array(rss_full_mat, mask=rss_full_mat < -199)
 
+            val_sequence = np.linspace(-100, -20, 80/5+1)
 
-
-            xx = np.reshape(x, data_shape)
-            yy = np.reshape(y, data_shape)
-            rss = np.reshape(rss_mean, data_shape)
-
-            val_sequence = np.linspace(-100,-20, 80/5+1)
-            CS = ax.contour(xx, yy, rss, val_sequence)
+            CS = ax.contour(wp_matx, wp_maty, rss_full_mat, val_sequence)
             ax.clabel(CS, inline=0, fontsize=10)
             for itx_plot in analyze_tx:
                 ax.plot(txpos[itx_plot-1, 0], txpos[itx_plot-1, 1], 'or')
@@ -491,5 +517,3 @@ def lambertloc(rss, alpha, gamma):
     """
     z = 20 / (np.log(10) * alpha) * lambertw(np.log(10) * alpha / 20 * np.exp(-np.log(10) / 20 * (rss + gamma)))
     return z.real  # [mm]
-
-
