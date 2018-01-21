3afb5090c8e9b0171fe2d7b93f9eef79881a9174
Daniel Duecker <daniel.duecker@tuhh.de>
2018-01-21 19:09:39 +0100

    moved socket class to own python file

--- socket_server.py
+++ socket_server.py
@@ -1,34 +1,40 @@
 import socket
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-ip = '134.28.27.35' #socket.gethostbyname(socket.gethostname())
-port = 1234
+class SocServer(object):
+    def __init__(self, ip_server, port_server):
+        self.__server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__ip_server = ip_server
+        self.__port_server = port_server
 
-address = (ip, port)
+        self.__server.bind((self.__ip_server, self.__port_server))
+        self.__server.listen(1)
+        print('Server: Started listening on ip: ' + self.__ip_server + ' on port: ' + str(self.__port_server))
+
+        print('...\nWaiting for client to connect...')
+        self.__incoming_client, self.__incoming_address = self.__server.accept()
+        print('Server: Got connection from ip: ' + self.__incoming_address[0] + ' on port: ' + self.__incoming_address[1])
+
+    def soc_get_data_from_client(self):
+
+        new_data = self.__incoming_client.recv(1024)
+        if new_data == 'disconnect':
+            print('Received command to disconnect!')
+            self.__server.close()
+        else:
+            print('Server: Received data: ' + new_data)
+
+        return new_data
 
 
+class SocClient(object):
+    def __init__(self, ip_server, port_server):
+        self.__client = socket.socket()
+        self.__ip_server = ip_server
+        self.__port_server = port_server
 
-server.bind(address)
-print('Server data = ' + ip + ' port = ' + str(port))
-server.listen(1)
+        self.__client.connect((self.__ip_server, self.__port_server))
 
+    def soc_send_data_so_server(self, data):
 
-client, addr = server.accept()
-
-print(addr)
-
-while True:
-    data = client.recv(1024)
-    print("Received: " + data + " from client!")
-    if (data == "Hello Server"):
-        client.send("Hello Client")
-        print("Processing done \n Reply sent!")
-    elif(data == "disconnect"):
-        client.send("Goodbye")
-        client.close()
-        break
-    else:
-        client.send("Data was invalid!")
-        print("Processing done - Invalid data \n Reply sent!")
-
+        self.__client.send(data)
--- start_wilhelmsburg.py
+++ start_wilhelmsburg.py
@@ -5,25 +5,27 @@ import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import gantry_control as gc
 
-Rf = rf.RfEar(434.2e6, 1e5)
+#Rf = rf.RfEar(434.2e6, 1e5)
 
 freq_1tx = [433.88e6]
 tx_1pos = [0,0]
-Rf.set_txparams(freq_1tx, tx_1pos)
+#Rf.set_txparams(freq_1tx, tx_1pos)
 
 tx_alpha = [0.01149]
 tx_gamma = [-8.52409]
-Rf.set_calparams(tx_alpha, tx_gamma)
+#Rf.set_calparams(tx_alpha, tx_gamma)
 
 
 #Rf.plot_power_spectrum_density()
-Rf.plot_txrss_live()
+#Rf.plot_txrss_live()
 
 
 #Rf.manual_calibration_for_one_tx('2018_01_18_messun_06.txt', 20)  # meastime = 5s
 
 #def analyze_measdata_from_file_1tx():
 
+
+my
 
 
 
