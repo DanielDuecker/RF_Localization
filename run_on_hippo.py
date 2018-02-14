63319eddbb40ad0e9bacbbbaf619fbedfd2a9576
Daniel Duecker <daniel.duecker@tuhh.de>
2018-02-14 10:47:27 +0100

    read yaw data from pixracer - run on hippo

--- run_on_hippo.py
+++ run_on_hippo.py
@@ -25,6 +25,16 @@ while True:
 
     """ Data Transmission to base station """
     yaw_rad = 0.0
+
+    filename = "home/pi/src/mavlink_communication_c_uart/YAW_r_des.txt"
+    readdata = []
+    with open(filename) as f:
+        for line in f:
+            readdata.append([float(n) for n in line.strip().split(',')])
+        f.close
+
+    print(readdata)
+
     data_list = [runtime, k, EKF.get_x_est(), EKF.get_z_meas(), EKF.get_y_est(), yaw_rad]
     soc_client.soc_process_server_request(data_list)
 
